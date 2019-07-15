766a4f3cbe914649b3a2d75ef7afe7a9614c7875
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2019-07-16 00:19:21 +0100

    add occlude remove others scripts

--- train_pixelcnn2.py
+++ train_pixelcnn2.py
@@ -133,6 +133,10 @@ def main():
     train_dataset = train_dataset.shuffle(buffer_size=train_buf)
     train_dataset = train_dataset.batch(batch_size)
 
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test_quantised.astype('float32')/(q_levels-1), x_test_quantised))
+    test_dataset = test_dataset.batch(batch_size)
+
+
     # https://github.com/RishabGoel/PixelCNN/blob/master/pixel_cnn.py
     # https://github.com/jonathanventura/pixelcnn/blob/master/pixelcnn.py
     n_channel = 1
@@ -200,127 +204,31 @@ def main():
             plt.yticks(np.array([]))
     plt.show()
 
-# ----------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------
+    test_loss = []
+    for batch_x, batch_y in test_dataset:
+        logits = pixelcnn(batch_x, training=False)
+
+        logits = tf.reshape(logits, [-1, 28, 28, q_levels, n_channel])  # shape [N,H,W,DC] -> [N,H,W,D,C]
+        logits = tf.transpose(logits, perm=[0, 1, 2, 4, 3])  # shape [N,H,W,D,C] -> [N,H,W,C,D]
+
+        loss = compute_loss(tf.one_hot(batch_y, q_levels), logits)
+        test_loss.append(loss)
+    print('nll:{:}'.format(np.array(test_loss).mean()))
+    print('bits/dim:{:}'.format(np.array(test_loss).mean()/(28*28)))
 
 
-
-#
-# https://github.com/RishabGoel/PixelCNN/blob/master/pixel_cnn.py
-# https://github.com/jonathanventura/pixelcnn/blob/master/pixelcnn.py
-"""Combined layer"""
-combined_stack = Conv2D(x_h, feature_maps, feature_maps, [1, 1, feature_maps, feature_maps], border_mode="valid")
-if q_levels != None:
-    out_dim = input_dim
-else:
-    out_dim = input_dim * q_levels
-
-combined_stack_final = Conv2D(combined_stack.get_output(), feature_maps, out_dim, [1, 1, feature_maps, out_dim],
-                              border_mode="valid")
-
-pre_final_output = tf.traanspose(combined_stack_final.get_output(), perm=[0, 2, 3, 1])
-if q_levels:
-    old_shape = pre_final_output.get_shape().as_list()
-    self.Y = tf.reshape(pre_final_output, shape=[old_shape[0], old_shape[1], old_shape[2] // q_levels, -1])
-else:
-    self.Y = pre_final_output
-
-
-
-https://github.com/rampage644/wavenet/commit/bbe8e3a98e48e3c3687d8ec9e342ef22cf530ac2
-nll = F.sigmoid_cross_entropy(y, F.cast(x, 'i'))
-
-
-# https://github.com/Joluo/PixelRNN/blob/master/tf/pixel_rnn.py
-logits = conv2d(output, hidden_dim, 1, 1, 'b', 'output_conv3')
-self.output = tf.nn.sigmoid(logits)
-
-# self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-#    logits=logits, labels=self.inputs, name='loss'))
-# self.loss = tf.reduce_mean(tf.contrib.keras.metrics.binary_crossentropy(self.inputs, self.output))
-self.loss = tf.reduce_mean(
-    -(tf.multiply(self.inputs, tf.log(self.output)) + tf.multiply(1 - self.inputs, tf.log(1 - self.output))))
-
-
-# https://github.com/singh-hrituraj/PixelCNN-Pytorch
-
-# https://github.com/rampage644/wavenet/blob/master/wavenet/models.py
-nll = F.softmax_cross_entropy(y, t, normalize=True)
-chainer.report({'nll': nll, 'bits/dim': nll / dims}, self)
-
-
-
-
-https://github.com/jakebelew/gated-pixel-cnn/blob/master/network.py
-if (num_channels > 1):
-    self.logits = tf.reshape(self.logits,
-                             [-1, height, width, q_levels, num_channels])  # shape [N,H,W,DC] -> [N,H,W,D,C]
-    self.logits = tf.transpose(self.logits, perm=[0, 1, 2, 4, 3])  # shape [N,H,W,D,C] -> [N,H,W,C,D]
-
-flattened_logits = tf.reshape(self.logits, [-1, q_levels])  # [N,H,W,C,D] -> [NHWC,D]
-target_pixels_loss = tf.reshape(self.target_pixels, [-1])  # [N,H,W,C] -> [NHWC]
-
-logger.info("Building loss and optims")
-self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-    flattened_logits, target_pixels_loss))
-
-flattened_output = tf.nn.softmax(flattened_logits)  # shape [NHWC,D], values [probability distribution]
-self.output = tf.reshape(flattened_output, [-1, height, width, num_channels,
-                                            q_levels])  # shape [N,H,W,C,D], values [probability distribution]
-
-
-def predict(self, images):
-    '''
-    images # shape [N,H,W,C]
-    returns predicted image # shape [N,H,W,C]
-    '''
-    # self.output shape [NHWC,D]
-    pixel_value_probabilities = self.sess.run(self.output, {
-        self.inputs: images})  # shape [N,H,W,C,D], values [probability distribution]
-
-    # argmax or random draw # [NHWC,1]  quantized index - convert back to pixel value
-    pixel_value_indices = np.argmax(pixel_value_probabilities,
-                                    4)  # shape [N,H,W,C], values [index of most likely pixel value]
-    pixel_values = np.multiply(pixel_value_indices, ((self.pixel_depth - 1) / (self.q_levels - 1)))  # shape [N,H,W,C]
-
-    return pixel_values
-
-
-def test(self, images, with_update=False):
-    if with_update:
-        _, cost = self.sess.run([self.optim, self.loss],
-                                {self.inputs: images[0], self.target_pixels: images[1]})
-    else:
-        cost = self.sess.run(self.loss, {self.inputs: images[0], self.target_pixels: images[1]})
-    return cost
-
-
-def generate_from_occluded(self, images, num_generated_images, occlude_start_row):
-    samples = np.copy(images[0:num_generated_images, :, :, :])
+    occlude_start_row = 14
+    num_generated_images = 100
+    samples = np.copy(x_test_quantised[0:num_generated_images, :, :, :]).astype('float32')
     samples[:, occlude_start_row:, :, :] = 0.
+    samples_labels = (np.ones((100, 1)) * 8).astype('int32')
 
-    for i in xrange(occlude_start_row, self.height):
-        for j in xrange(self.width):
-            for k in xrange(self.channel):
-                next_sample = self.predict(samples) / (self.pixel_depth - 1.)  # argmax or random draw here
-                samples[:, i, j, k] = next_sample[:, i, j, k]
-
-    return samples
-
-
-def generate(self, images):
-    samples = images[0:9, :, :, :]
-    occlude_start_row = 18
-    samples[:, occlude_start_row:, :, :] = 0.
-
-    for i in xrange(occlude_start_row, self.height):
-        for j in xrange(self.width):
-            for k in xrange(self.channel):
-                next_sample = self.predict(samples) / (self.pixel_depth - 1.)  # argmax or random draw here
-                samples[:, i, j, k] = next_sample[:, i, j, k]
-
-
-return samples
+    for i in range(occlude_start_row, 28):
+        for j in range(28):
+            A = pixelcnn([samples, samples_labels])
+            A = tf.reshape(A, [-1, 28, 28, q_levels, n_channel])  # shape [N,H,W,DC] -> [N,H,W,D,C]
+            A = tf.transpose(A, perm=[0, 1, 2, 4, 3])  # shape [N,H,W,D,C] -> [N,H,W,C,D]
+            B = tf.nn.softmax(A)
+            next_sample = B[:, i, j, 0, :]
+            samples[:, i, j, 0] = sample_from(next_sample.numpy()) / (q_levels - 1)
+            print("{} {}: {}".format(i, j, sample_from(next_sample.numpy())[0]))
